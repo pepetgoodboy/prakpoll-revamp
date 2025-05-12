229fb2ad5e038aaaeaed7ece1af7d6ca5ee21a0b
pepetgoodboy <iqbaltw127@gmail.com>
2025-05-12 12:13:04 +0700

    fix: change gitignore for testing

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("User Registration", () => {
+  it("should register a new user successfully", () => {
+    cy.visit("http://localhost:3000/register");
+
+    cy.get('input[name="npm"]').type("0425002");
+    cy.get('input[name="password"]').type("bagus123");
+    cy.get('input[name="verifCode"]').type("ad3ebf");
+
+    cy.get('button[type="submit"]').click();
+
+    cy.wait(1000);
+    cy.contains("Registrasi berhasil").should("be.visible");
+
+    cy.url().should("include", "/login");
+    cy.contains("Masuk");
+  });
+});
